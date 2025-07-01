07a257dcb0b9521a2e947afae28395d8eca62140
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-07-01 11:50:26 -0400

    criando tela inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>teste</h1>
+  </div>
 </template>
