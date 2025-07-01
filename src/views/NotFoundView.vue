be868701d1097f3a29b843a40cd5209ae5327985
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-07-01 11:52:32 -0400

    criando pagina de nao encontrado

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>404 - Página Não Encontrada</h2>
+    <p>A página que você está procurando não existe.</p>
+    <router-link to="/">Voltar para a Home</router-link>
+  </div>
+</template>
