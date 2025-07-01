8e51cb70b1d82829ef913eca1d5bf1c0a685f2e2
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-07-01 11:51:03 -0400

    configurando layout do front

---
+++ src/shared/components/LayoutComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import Header from '@/shared/components/HeaderComponent.vue'
+</script>
+
+<template>
+  <main>
+    <Header/>
+
+    <RouterView/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
