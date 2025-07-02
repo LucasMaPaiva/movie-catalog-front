97d56888024971a2a5c8bef536188ef5f64d3e7f
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-07-02 00:09:49 -0400

    criando service para buscar filmes

---
+++ src/shared/utils/SearchFilms.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+/**
+ * @param {string} query
+ * @returns {Promise<Array>}
+ * @throws {Error}
+ */
+export const fetchMoviesSearch = async (query) => {
+  try {
+    const response = await axios.get(`http://localhost:8089/api/v1/movies/search?query=${query}`);
+    return response.data.results || [];
+  } catch (err) {
+    console.error('Erro na chamada da API fetchMoviesSearch:', err);
+    throw new Error('Não foi possível carregar os filmes da busca. Tente novamente mais tarde.');
+  }
+};
